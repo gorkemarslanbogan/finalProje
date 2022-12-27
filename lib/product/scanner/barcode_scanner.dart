92a588188be922dc2e74f7e2e9a4c1a2e00861f4
gorkemarslanbogan <mavisimsek940@gmail.com>
2022-12-27 20:14:07 +0300

    barcode scanner wrote

--- lib/product/scanner/barcode_scanner.dart
+++ lib/product/scanner/barcode_scanner.dart
@@ -1,10 +1,26 @@
 
+import 'package:final_project/product/scanner/scanner_exception.dart';
 import 'package:flutter_barcode_scanner/flutter_barcode_scanner.dart';
 
 class BarcodeScanner {
 BarcodeScanner._();
-static Future<String> ScanBarcode() async {
-  String barcodeScanRes = await FlutterBarcodeScanner.scanBarcode("#ff6666","Kapat",true,ScanMode.BARCODE);
-  return barcodeScanRes;
+static Future<String> ScanBarcode(ScannerType type) async {
+  if(type == ScannerType.ORDERS){
+    return _scannerFun();
+  }
+  else if(type == ScannerType.PRODUCT){
+   return  _scannerFun();
+  }
+  else {
+    throw BarcodeScannerException();
+  }
 }
+static Future<String> _scannerFun() async {
+    String barcodeScanRes = await FlutterBarcodeScanner.scanBarcode("#ff6666","Kapat",true,ScanMode.BARCODE);
+    return barcodeScanRes;
+}
+}
+
+enum ScannerType {
+  ORDERS,PRODUCT
 }
